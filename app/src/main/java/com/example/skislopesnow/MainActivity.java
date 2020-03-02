e807be05cbef6908af06c556aaee9544e7d66269
Tori Tenney <toritenney@Andrews-MacBook-Pro.local>
2020-03-01 18:18:09 -0700

    adapting recyclerview from https://androidwave.com/android-recyclerview-example-best-practices/

--- app/src/main/java/com/example/skislopesnow/MainActivity.java
+++ app/src/main/java/com/example/skislopesnow/MainActivity.java
@@ -1,55 +1,71 @@
 package com.example.skislopesnow;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.skislopesnow.utils.CommonUtils;
+import com.example.skislopesnow.utils.DividerItemDecoration;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+public class MainActivity extends AppCompatActivity implements SportAdapter.Callback {
 
-public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.mRecyclerView)
+    RecyclerView mRecyclerView;
+    SportAdapter mSportAdapter;
+
+    LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        setUp();
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    private void setUp() {
+        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        Drawable dividerDrawable = ContextCompat.getDrawable(this, R.drawable.divider_drawable);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(dividerDrawable));
+        mSportAdapter = new SportAdapter(new ArrayList<>());
+
+        prepareDemoContent();
+    }
+
+    private void prepareDemoContent() {
+        CommonUtils.showLoading(MainActivity.this);
+        new Handler().postDelayed(() -> {
+            //prepare data and show loading
+            CommonUtils.hideLoading();
+            ArrayList<Sport> mSports = new ArrayList<>();
+            String[] sportsList = getResources().getStringArray(R.array.sports_titles);
+            String[] sportsInfo = getResources().getStringArray(R.array.sports_info);
+            String[] sportsImage = getResources().getStringArray(R.array.sports_images);
+            for (int i = 0; i < sportsList.length; i++) {
+                mSports.add(new Sport(sportsImage[i], sportsInfo[i], "News", sportsList[i]));
             }
-        });
+            mSportAdapter.addItems(mSports);
+            mRecyclerView.setAdapter(mSportAdapter);
+        }, 2000);
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onEmptyViewRetryClick() {
+        prepareDemoContent();
     }
 }
